9d5aa8a6634b304c09c8e8a569e0d51ffe9844ab
Abhay Dixit <dixitshivam534@gmail.com>
2024-09-16 23:45:36 +0530

    navbar in react

--- React First/src/App.jsx
+++ React First/src/App.jsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
-import './App.css'
 import "./style.css"
-import img from "./assets/img1.jpeg"
 function App() {
 
-const btnCss={
-  backgroundColor: 'blue',
-  color: 'white',
-  padding: '10px 20px',
-  border: 'none',
-  borderRadius: '5px',
-  cursor: 'pointer'
-
+const navbar={
+    display:"flex",
+    justifyContent:"space-evenly",
+    listStyleType:"none",
+    backgroundColor:"#333",
+    color:"#fff",
+    padding:"10px",
+    width:"100%",
+    margin:"0px"
+}
+const liItemStyle={
+  backgroundColor:"rgba(255, 255, 255, 0)",
+  cursor:"pointer",
+  fontSize:"20px",
 }
   return (
     <>
-      <img src={img}/>
-          <br />
-      <h1 style={{fontSize:"50px",color:"Red",fontWeight:"800"}}>Hello World!</h1>
-      <button style={btnCss}>Click Here</button>
-      <button id="new">External Click Here</button>
+     <ul style={navbar}>
+      <li style={liItemStyle}>Home</li>
+      <li style={liItemStyle}>Services</li>
+      <li style={liItemStyle}>Contact</li>
+      <li style={liItemStyle}>About us</li>
+      <li style={liItemStyle}>Logout</li>
+     </ul>
     </>
   )
 }

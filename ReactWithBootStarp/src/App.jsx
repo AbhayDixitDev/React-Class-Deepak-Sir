f83e923b0af932e2492a4b620cd1bd0227b34d63
Abhay Dixit <dixitshivam534@gmail.com>
2024-09-19 23:57:44 +0530

    new project

---
+++ ReactWithBootStarp/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar1 from "./components/Navbar"
+import Footer from "./components/Footer"
+const App=()=>{
+  return (
+    <>
+    <Navbar1></Navbar1>
+    <Footer></Footer>
+    </> 
+  )
+}
+
+export default App

98430877ff81c5cb5627f8c682fb6274f11c4923
Abhay Dixit <dixitshivam534@gmail.com>
2024-09-20 00:14:41 +0530

    new changes added navbar footer and carousel

--- ReactWithBootStarp/src/App.jsx
+++ ReactWithBootStarp/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar1 from "./components/Navbar"
+import Carousel1 from "./pages/Carousal"
 import Footer from "./components/Footer"
 const App=()=>{
   return (
     <>
     <Navbar1></Navbar1>
+    <Carousel1></Carousel1>
     <Footer></Footer>
     </> 
   )
--- ReactWithBootStarp/src/pages/Carousal.jsx
+++ ReactWithBootStarp/src/pages/Carousal.jsx
@@ -1,25 +1,31 @@
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import img1 from "../assets/img1.avif"
+import img2 from "../assets/img2.webp"
+import img3 from "../assets/img3.jpeg"
+
 
 function Carousel1() {
   return (
     <Carousel slide={false}>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <img className="d-block w-75 h-75 mx-auto"src={img1} alt="First slide"/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        {/* <ExampleCarouselImage text="Second slide" /> */}
+        <img className="d-block w-75 h-75 mx-auto"src={img2} alt="First slide"/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        {/* <ExampleCarouselImage text="Third slide" /> */}
+        <img className="d-block w-50 h-50 mx-auto"src={img3} alt="First slide"/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>

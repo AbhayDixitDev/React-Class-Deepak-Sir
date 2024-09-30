12d41b6f75a185e54e54ccb1c83eeecf64c35ce7
Abhay Dixit <dixitshivam534@gmail.com>
2024-09-30 19:25:06 +0530

    react map

--- React First/src/App.jsx
+++ React First/src/App.jsx
@@ -1,28 +1,19 @@
-import Navbar from "./Components/Navbar.jsx";
-import Hero from "./Pages/Hero.jsx";
-import AboutMe from "./Pages/AboutMe.jsx";
-import Skills from "./Pages/Skills.jsx";
-import Testimonials from "./Pages/Testimonials.jsx";
-import Form from "./Pages/Form.jsx";
-import Contact from "./Pages/Contact.jsx";
-import Footer from "./Components/Footer.jsx";
-import Navbar1 from "./Components/navbar1.jsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
+const arr=["ram","shyam","gopal","mohan","krishna"];
 
-function App() {
-
+const App=()=> {
+const ans=arr.map((key)=>{
+  return (
+  <li><h3>{key}</h3></li>
+  )
+})
 
   return (
     <>
-    <Navbar1></Navbar1>
-     <Navbar></Navbar>
-     <Hero></Hero>
-     <AboutMe></AboutMe>
-     <Skills></Skills>
-     <Testimonials></Testimonials>
-     <Form></Form>
-     <Contact></Contact>
-     <Footer></Footer>
+    <ol>
+       {ans}
+    </ol>
+   
+    
     </>
   )
 }

620aa23cb5106c4d8022ba455767175c96f4ea46
Abhay Dixit <dixitshivam534@gmail.com>
2024-10-05 14:32:13 +0530

    useEffect in react

--- React-App5/src/App.jsx
+++ React-App5/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useState, useEffect } from 'react'
+const App = () => {
+  const [name, setName] = useState('')
+  const [name1, setName1] = useState('')
+  useEffect(() => {
+    console.log('name', name)
+    console.log('name1', name1)
+  }, [name])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <input type="text" onChange={(e) => setName1(e.target.value)} />
+    </div>
+
   )
+
 }
 
 export default App

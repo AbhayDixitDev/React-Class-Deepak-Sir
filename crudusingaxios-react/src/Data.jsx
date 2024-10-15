dd599006e4162c24f470db9e23afe4a4587c5948
Abhay Dixit <dixitshivam534@gmail.com>
2024-10-15 13:12:44 +0530

    data map axios get api

--- crudusingaxios-react/src/Data.jsx
+++ crudusingaxios-react/src/Data.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Card from 'react-bootstrap/Card';
 import axios from 'axios'
 const Data = () => {
     const [jsonData, setJsonData] = useState([])
@@ -9,7 +10,26 @@ const Data = () => {
 
   return (
     <>
-   { console.log(jsonData)}
+    {
+      jsonData.map((item)=>{
+        return(
+          <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={item.image} />
+          <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>
+              {jsonData.age}
+            </Card.Text>
+            <Card.Title>{item.city}</Card.Title>
+            <Card.Title>{item.email}</Card.Title>
+          </Card.Body>
+        </Card>
+        )
+      })
+      
+    }
+    
+
     </>
   )
 }

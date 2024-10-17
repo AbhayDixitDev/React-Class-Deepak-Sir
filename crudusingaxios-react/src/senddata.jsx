39dabdca45aa9e11540f6ba76dbc33c98523d7c0
Abhay Dixit <dixitshivam534@gmail.com>
2024-10-17 15:58:12 +0530

    search and update

--- crudusingaxios-react/src/senddata.jsx
+++ crudusingaxios-react/src/senddata.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Button from 'react-bootstrap/Button';
-
+import './SendData.css'; // Import the external CSS file
 
 const SendData = () => {
-    const [jsonData, setJsonData] = useState([])
-    const [show, setShow] = useState(false)
-    const [updateid, setUpdateid] = useState(null)
-    const [updform, setUpdform] = useState(false)
+    const [jsonData, setJsonData] = useState([]);
+    const [show, setShow] = useState(false);
+    const [updateid, setUpdateid] = useState(null);
+    const [updform, setUpdform] = useState(false);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        axios.get('http://localhost:4000/info').then((items)=>setJsonData(items.data));
-    }, [])   
+        axios.get('http://localhost:4000/info').then((items) => setJsonData(items.data));
+    }, []);
 
     const [userdata, setUserdata] = useState({
         name: "",
@@ -20,102 +21,106 @@ const SendData = () => {
         city: "",
         email: ""
     });
+
+    let filteredData1 = jsonData;
+    let showdata = search === "" ? filteredData1 : filteredData1.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+
     const handleinput = (e) => {
         let key = e.target.name;
         let value = e.target.value;
-        setUserdata({ 
-            ...userdata, 
-            ...{ [key]: value } 
-        });
-        console.log(userdata);
+        setUserdata({ ...userdata, [key]: value });
     }
+
     const updatevalue = (e) => {
         let key = e.target.name;
         let value = e.target.value;
-        setUserdata({ 
-            ...userdata, 
-            ...{ [key]: value } 
-        });
-        updateform()
+        setUserdata({ ...userdata, [key]: value });
+        updateform();
     }
+
     const submitdata = () => {
         axios.post("http://localhost:4000/info", JSON.stringify(userdata))
-        .then(()=>{window.alert("Data sent successfully")})
-        .then(()=>{setUserdata({
-            name: "",
-            image: "",
-            age: "",
-            city: "",
-            email: ""
-        })})
+            .then(() => { window.alert("Data sent successfully") })
+            .then(() => {
+                setUserdata({
+                    name: "",
+                    image: "",
+                    age: "",
+                    city: "",
+                    email: ""
+                });
+            });
     }
+
     const deleteuser = (id) => {
-        axios.delete(`http://localhost:4000/info/${id}`).then(()=>{window.alert("Data deleted successfully")})
+        axios.delete(`http://localhost:4000/info/${id}`).then(() => { window.alert("Data deleted successfully") });
     }
+
     const updateform = (arg) => {
-        alert(arg)
-        setUpdateid(arg)
-        setUpdform(!updform)
+        setUpdateid(arg.id);
+        setUpdform(!updform);
         setUserdata({
             name: arg.name,
             image: arg.image,
             age: arg.age,
             city: arg.city,
             email: arg.email
-        })
-        alert(JSON.stringify(userdata))
+        });
     }
+
     const updateuser = () => {
-        axios.put(`http://localhost:4000/info/${updateid}`, JSON.stringify(userdata)).then(()=>{window.alert("Data updated successfully")})
+        axios.put(`http://localhost:4000/info/${updateid}`, JSON.stringify(userdata)).then(() => { window.alert("Data updated successfully") });
     }
+
     return (
         <>
-        <Button variant="primary" style={{margin:"2rem auto",display:"block"}} onClick={()=>setShow(!show)}>Add User</Button>
-       <form style={{display:show?"flex":"none",flexDirection:"column",alignItems:"center",justifyContent:"center",margin:"2rem auto",border:"1px solid black",padding:"1rem",width:"20rem"}}>
-       <input type="text" name="name" placeholder="Enter your name" value={userdata.name} onChange={handleinput} /> <br />
-        <input type="text" name="image" placeholder="Enter your image url" value={userdata.image} onChange={handleinput} /> <br />
-        <input type="text" name="age" placeholder="Enter your age" value={userdata.age} onChange={handleinput} /> <br />
-        <input type="text" name="city" placeholder="Enter your city" value={userdata.city} onChange={handleinput} /> <br />
-        <input type="text" name="email" placeholder="Enter your email" value={userdata.email} onChange={handleinput} /> <br />
-        <button onClick={submitdata}>Submit</button>
-       </form>
-       <form style={{display:updform?"flex":"none",flexDirection:"column",alignItems:"center",justifyContent:"center",margin:"2rem auto",border:"1px solid black",padding:"1rem",width:"20rem"}}>
-       <input type="text" name="name" placeholder="Enter your name" value={userdata.name} onChange={updatevalue} /> <br />
-        <input type="text" name="image" placeholder="Enter your image url" value={userdata.image} onChange={updatevalue} /> <br />
-        <input type="text" name="age" placeholder="Enter your age" value={userdata.age} onChange={updatevalue} /> <br />
-        <input type="text" name="city" placeholder="Enter your city" value={userdata.city} onChange={updatevalue} /> <br />
-        <input type="text" name="email" placeholder="Enter your email" value={userdata.email} onChange={updatevalue} /> <br />
-        <button onClick={updateuser}>Update</button>
-       </form>
+            <input type="text" placeholder="Search by name" onChange={(e) => setSearch(e.target.value)} id="search"  />
+            <Button variant="primary" className="button" onClick={() => setShow(!show)}>Add User</Button>
+            
+            <form className={`form ${show ? '' : 'hidden'}`}>
+                <input type="text" name="name" placeholder="Enter your name" value={userdata.name} onChange={handleinput} /><br />
+                <input type="text" name="image" placeholder="Enter your image url" value={userdata.image} onChange={handleinput} /><br />
+                <input type="text" name="age" placeholder="Enter your age" value={userdata.age} onChange={handleinput} /><br />
+                <input type="text" name="city" placeholder="Enter your city" value={userdata.city} onChange={handleinput} /><br />
+                <input type="text" name="email" placeholder="Enter your email" value={userdata.email} onChange={handleinput} /><br />
+                <button onClick={submitdata}>Submit</button>
+            </form>
 
-        <table style={{margin:"2rem auto",border:"1px solid black",padding:"1rem",width:"40rem"}}>
-            <tr style={{border:"1px solid black"}}>
-                <th style={{border:"1px solid black"}}>Name</th>
-                <th style={{border:"1px solid black"}}>Image</th>
-                <th style={{border:"1px solid black"}}>Age</th>
-                <th style={{border:"1px solid black"}}>City</th>
-                <th style={{border:"1px solid black"}}>Email</th>
-                <th style={{border:"1px solid black"}}>Action</th>
-            </tr>
-       {
-        jsonData.map((item)=>(
-            <tr style={{border:"1px solid black"}}>
-                <td style={{border:"1px solid black"}}>{item.name}</td>
-                <td style={{border:"1px solid black",backgroundImage:`url(${item.image})`,backgroundSize:"cover",backgroundPosition:"center",width:"100px",height:"100px"}}></td>
-                <td style={{border:"1px solid black"}}>{item.age}</td>
-                <td style={{border:"1px solid black"}}>{item.city}</td>
-                <td style={{border:"1px solid black"}}>{item.email}</td>
-                <td style={{border:"1px solid black"}}><Button variant="danger" onClick={()=>deleteuser(item.id)}>Delete</Button></td>
-                <td style={{border:"1px solid black"}}><Button variant="warning" onClick={()=>updateform(item)}>Update</Button></td>
-                
-            </tr>
-          )
-        )
-        
-      }
-      </table>
+            <form className={`form ${updform ? '' : 'hidden'}`}>
+                <input type="text" name="name" placeholder="Enter your name" value={userdata.name} onChange={updatevalue} /><br />
+                <input type="text" name="image" placeholder="Enter your image url" value={userdata.image} onChange={updatevalue} /><br />
+                <input type="text" name="age" placeholder="Enter your age" value={userdata.age} onChange={updatevalue} /><br />
+                <input type="text" name="city" placeholder="Enter your city" value={userdata.city} onChange={updatevalue} /><br />
+                <input type="text" name="email" placeholder="Enter your email" value={userdata.email} onChange={updatevalue} /><br />
+                <button onClick={updateuser}>Update</button>
+            </form>
+
+            <table className="table">
+                <tr key="header">
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Age</th>
+                    <th>City</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+                {
+                    showdata.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td style={{backgroundImage:`url(${item.image})`,backgroundSize:"cover",backgroundPosition:"center",height:"200px"}}></td>
+                            <td>{item.age}</td>
+                            <td>{item.city}</td>
+                            <td>{item.email}</td>
+                            <td><Button variant="danger" style={{backgroundColor:"red"}} onClick={() => deleteuser(item.id)}>Delete</Button>
+                            <Button variant="warning" style={{backgroundColor:"yellow"}} onClick={() => updateform(item)}>Update</Button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </table>
         </>
-    )
-}
+    );
+};
 
 export default SendData;

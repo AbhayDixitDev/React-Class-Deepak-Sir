eae318e1055a06a6c9499a9f47d4eb6623efd43c
Abhay Dixit <dixitshivam534@gmail.com>
2024-10-17 17:32:41 +0530

    data deploy vercel

--- crudusingaxios-react/vite.config.js
+++ crudusingaxios-react/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./styles/variables";`,
+      },
+    },
+  },
   plugins: [react()],
 })

c19c27cda218c51b7ab3a49abb55a0dd4238b973
Abhay Dixit <dixitshivam534@gmail.com>
2024-10-17 17:39:27 +0530

    data deploy vercel

--- crudusingaxios-react/vite.config.js
+++ crudusingaxios-react/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist',
-    publicDir: 'dist', // Set the public directory to "dist"
+    outDir: 'dist', // Output directory for bundled files
   },
   css: {
     preprocessorOptions: {
